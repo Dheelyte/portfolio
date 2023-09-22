8f870f9ad14641d2963a27dcd4519be21d0c2f32
Olu-Olagbuji Delight <olagbujidelight@gmail.com>
2023-09-22 13:51:52 +0100

    Add Portfolio project files

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -46,7 +46,7 @@ export default function Projects () {
                         popular points of interest around you. It also shows you details and pictures of these places.
                         It's like your personal online tour guide.
                     </p>
-                    <a href="https://github.com/Dheelyte/tourify">
+                    <a href="https://tourify-nbzf.onrender.com/">
                         <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2"><path d="M18 13v6a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V8a2 2 0 0 1 2-2h6"></path><polyline points="15 3 21 3 21 9"></polyline><line x1="10" y1="14" x2="21" y2="3"></line></svg>    
                     </a>
                     <a href="https://github.com/Dheelyte/tourify">
